367d386aa28dd154e202e72cee601fdb8f7078c5
Fabiano Evaristo <fabianoevaristo007@gmail.com>
2022-06-12 14:42:06 -0300

    Att QUERY

--- Database/Exemplo_AtualizarUsuario.sql
+++ Database/Exemplo_AtualizarUsuario.sql
@@ -1,16 +1,31 @@
 ﻿DECLARE @Id int, @Nome varchar(30), @Email varchar(30), @Cpf varchar(11), @DataNasc Date
 
-SET @Id = 2
+SET @Id = 3
 SET @DataNasc = '22/03/2001'
+SET @Cpf = '123456'
+SET @Email = 'testa@gmail.com'
 
 IF EXISTS (SELECT 1 FROM dbo.Usuarios WHERE id = @Id)
 BEGIN
+	IF(@Email is not null) AND EXISTS (SELECT 1 FROM dbo.Usuarios WHERE email = @Email)
+	BEGIN
+	PRINT 'EMAIL ' + @Email + ' EM USO'
+	RETURN
+	END
+
+	IF(@Cpf is not null) AND EXISTS (SELECT 1 FROM dbo.Usuarios WHERE cpf = @Cpf)
+	BEGIN
+	PRINT 'CPF ' + @Cpf + ' EM USO'
+	RETURN
+	END
+
 	UPDATE dbo.Usuarios 
 		SET 
 		nome = IsNull(@Nome, nome),
-		email = IsNull(@Email, email),
+		datanasc = IsNull(@DataNasc, datanasc),
 		cpf = IsNull(@Cpf, cpf),
-		datanasc = IsNull(@DataNasc, datanasc)
+		email = IsNull(@Email, email)
 	WHERE id = @id
+
 	PRINT 'Usuario ID Editado'
 END
