9255ea3196cf1422b98bb29b2dd1346c8ec09003
Fabiano Evaristo <fabianoevaristo007@gmail.com>
2022-06-12 02:11:24 -0300

    Tabela SQL - Exemplo

---
+++ Database/Exemplo_Tabela_Usuarios.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Usuarios] (
+    [id]       INT IDENTITY (1, 1) NOT NULL,
+    [nome]     VARCHAR (30) NOT NULL,
+    [email]    VARCHAR (30) NOT NULL,
+    [cpf]      VARCHAR (11) NOT NULL,
+    [senha]    VARCHAR (30) NOT NULL,
+    [datanasc] DATE         NOT NULL,
+);
